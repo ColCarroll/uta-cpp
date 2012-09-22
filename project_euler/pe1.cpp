c51217a3451ac1a28f5c2dc9a5f94d2c49ee30f1
Nicholas Zufelt <Nicholas@Nicholas-Zufelts-MacBook.local>
2012-09-21 20:55:03 -0500

    This is Nick's first attempt to commit.  It should commit my solution to project euler problem 1.  Commit date is 9/21/12 at 8:56P

---
+++ project_euler/pe1.cpp
@@ -0,0 +1,25 @@
+//  This is project euler problem 1 - Find the sum of all the 
+//  multiples of 3 or 5 below 1000..  
+//  You run it by using the compiler command g++
+//  Start time: 4:03
+//  Finish time: 4:16
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+  int s = 0;
+  
+  for (int i=1; i < 1000; i++)
+  {
+    if (i % 3 == 0 || i % 5 == 0)
+      {	
+        s = s + i;
+      }
+  }
+
+  cout << s << endl;
+
+  return 0;
+}
