a5ccd6917c5f593e39a40dbc8b8486a5d8ae27ad
neisenmenger <neisenmenger@gmail.com>
2012-11-12 17:41:18 -0800

    moved to the correct folder ... maybe Please enter the cMommit message for your changes. Lines starting

---
+++ example_code/2D_Arrays.cpp
@@ -0,0 +1,59 @@
+//============================================================================
+// Name        : 2D_Arrays.cpp
+// Author      : Nancy Eisenmenger
+
+
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int PrintArray(int **ary, int sizeX, int sizeY);
+
+int main() {
+	//cout << "" << endl; // prints
+	int sizeX, sizeY;
+	sizeX = 10;
+	sizeY = 10;
+
+	int** ary = new int*[sizeX]; // creates an array of pointers to array
+	for(int i = 0; i < sizeX; ++i)
+	{
+		ary[i] = new int[sizeY]; //sets each pointer to point to an array of ints
+		for(int j = 0; j< sizeY; ++j)
+		{
+			ary[i][j] = (i+1)*(j+1); // assigns a value to each element in the array
+		}
+	}
+
+	PrintArray(ary, sizeX, sizeY); //example of how to pass the array to a function (pass size of array with array)
+	return 0;
+}
+
+int PrintArray(int **ary, int sizeX, int sizeY)
+{
+	for(int i = 0; i < sizeX; ++i)
+	{
+
+		for(int j = 0; j< sizeY; ++j)
+		{
+			cout << ary[i][j] << " "; // prints each element in the array
+		}
+		cout << endl;
+	}
+	return 0;
+}
+
+/* Sample Output
+1 2 3 4 5 6 7 8 9 10
+2 4 6 8 10 12 14 16 18 20
+3 6 9 12 15 18 21 24 27 30
+4 8 12 16 20 24 28 32 36 40
+5 10 15 20 25 30 35 40 45 50
+6 12 18 24 30 36 42 48 54 60
+7 14 21 28 35 42 49 56 63 70
+8 16 24 32 40 48 56 64 72 80
+9 18 27 36 45 54 63 72 81 90
+10 20 30 40 50 60 70 80 90 100
+ */
+
